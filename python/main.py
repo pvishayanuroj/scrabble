e6f1707b6753d10968d6fa638a2b34b9bebce666
Paul Vishayanuroj <pvishayanuroj@gmail.com>
2024-02-17 09:15:59 +0900

    Refactored free functions into classes. Implemented state loading and printing.

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 import os
 from enum import Enum
+from typing import List
 
 class Tile(Enum):
     NORMAL = "--"
@@ -17,15 +18,89 @@ class Tile(Enum):
         raise ValueError(f"Invalid tile enum value: |{value}|")
 
 class Board:
-    def __init__(self, board, state = None):
-        self.board = board
-        if state is None:
-            self.state = []
-            for x in len(self.board):
-                self.state.append([''] * len(self.board))
-        else:
-            self.state = state
+    def __init__(self):
+        self.num_cols = 0
+        self.num_rows = 0
+        self.board = []
+        self.state = []
 
+    def init_board(self, filepath: str):
+        self.num_cols = None
+        self.board.clear()
+        with open(filepath, 'r') as file:
+            for line in file.readlines():
+                if line == '':
+                    continue
+                row = [Tile.from_string(tile) for tile in line.strip().upper().split(' ')]
+                if self.num_cols is None:
+                    self.num_cols = len(row)
+                elif self.num_cols != len(row):
+                    raise ValueError(f"Mismatched number of columns. Expected {self.num_cols}, got {len(row)}")
+                self.board.append(row)
+        self.num_rows = len(self.board)
+        self.init_state()
+        print(f"Initialized board: {self.num_rows} rows by {self.num_cols} cols")
+
+    def init_state(self):
+        self.state = [[''] * self.num_cols for _ in range(self.num_rows)]
+
+    def load_state(self, filepath: str):
+        if self.num_cols == 0 or self.num_rows == 0:
+            raise ValueError(f"Cannot load state without initializing the board.")
+        self.state = []
+        with open(filepath, 'r') as file:
+            for line in file.readlines():
+                if line == '':
+                    continue
+                row = []
+                for letter in line.strip().upper().split(' '):
+                    if letter == '-' or letter.isupper():
+                        row.append(letter)
+                    else:
+                        raise ValueError(f"Invalid input letter: {letter}")
+                if len(row) != self.num_cols:
+                    raise ValueError(f"Expected {self.num_cols} columns, got {len(row)}")
+                self.state.append(row)
+            if len(self.state) != self.num_rows:
+                raise ValueError(f"Expected {self.num_rows} rows, got {len(self.state)}")
+
+    def save(self, filepath: str):
+        pass
+
+    def __str__(self):
+        output = ''
+        for row in self.state:
+            output += ' '.join(map(lambda x: '-' if x == '' else x, row))
+            output += '\n'
+        return output
+
+
+def get_all_substrings(word: str) -> List[str]:
+    substrings = []
+    for substring_len in range(2, len(word)):
+        for start_index in range(len(word) - substring_len + 1):
+            substrings.append(word[start_index:(start_index + substring_len)])
+    return substrings
+
+class Dictionary:
+    def __init__(self):
+        self.words = set()
+        self.substrings = set()
+
+    def load(self, filepath: str):
+        with open(filepath, 'r') as file:
+            for line in file.readlines():
+                if line == '':
+                    continue
+                self.words.add(line.strip().upper())
+        self.preprocess()
+        print(f"Loaded {len(self.words)} words")
+        print(f"Preprocessed {len(self.substrings)} substrings")
+
+    def preprocess(self):
+        for word in self.words:
+            for substring in get_all_substrings(word):
+                self.substrings.add(substring)
 
 def read_directory_files(path):
     files = set()
@@ -33,38 +108,21 @@ def read_directory_files(path):
         files.add(filename)
     return files
 
-
-def read_dictionary(filepath):
-    words = set()
-    with open(filepath, 'r') as file:
-        for line in file.readlines():
-            if line == '':
-                continue
-            words.add(line.strip().upper())
-    return words
-
-
-def read_board(filepath):
-    board = []
-    with open(filepath, 'r') as file:
-        for line in file.readlines():
-            row = []
-            for tile in line.strip().upper().split(' '):
-                row.append(Tile.from_string(tile))
-            board.append(row)
-    return board
-
 def read_state(path):
     state = []
     return state
 
 PATH = '/Users/pvishayanuroj/projects/scrabble/dictionaries'
 
-DICTIONARY_PATH = '/Users/pvishayanuroj/projects/scrabble/dictionaries/10k-dictionary.txt'
+#DICTIONARY_PATH = '/Users/pvishayanuroj/projects/scrabble/dictionaries/10k-dictionary.txt'
+DICTIONARY_PATH = '/Users/pvishayanuroj/projects/scrabble/dictionaries/178k-dictionary.txt'
 BOARD_PATH = '/Users/pvishayanuroj/projects/scrabble/boards/official.txt'
+STATE_PATH = '/Users/pvishayanuroj/projects/scrabble/states/test.txt'
 
+board = Board()
+board.init_board(BOARD_PATH)
+board.load_state(STATE_PATH)
+print(board)
 #files = read_directory_files(PATH)
-words = read_dictionary(DICTIONARY_PATH)
-board = read_board(BOARD_PATH)
-print(f"Board size: {len(board)}")
-print(f"Dictionary length: {len(words)}")
+#dictionary = Dictionary()
+#dictionary.load(DICTIONARY_PATH)
