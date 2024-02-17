f73d901afd0d44da0673b3ea7e5167066425f1cb
Paul Vishayanuroj <pvishayanuroj@gmail.com>
2024-02-17 10:47:13 +0900

    Implemented a checker whether the state of the board is valid.

--- python/main.py
+++ python/main.py
@@ -2,6 +2,7 @@ import os
 from enum import Enum
 from typing import List
 
+
 class Tile(Enum):
     NORMAL = "--"
     DOUBLE_LETTER = "DL"
@@ -16,6 +17,40 @@ class Tile(Enum):
             if tile.value == value:
                 return tile
         raise ValueError(f"Invalid tile enum value: |{value}|")
+
+
+class Dictionary:
+    def __init__(self):
+        self.words = set()
+        self.substrings = set()
+
+    def load(self, filepath: str):
+        with open(filepath, 'r') as file:
+            for line in file.readlines():
+                if line == '':
+                    continue
+                self.words.add(line.strip().upper())
+        self.preprocess()
+        print(f"Loaded {len(self.words)} words")
+        print(f"Preprocessed {len(self.substrings)} substrings")
+
+    def preprocess(self):
+        for word in self.words:
+            for substring in get_all_substrings(word):
+                self.substrings.add(substring)
+
+    def is_word(self, value: str) -> bool:
+        return value in self.words
+
+
+class Position:
+    def __init__(self, row: int, col: int):
+        self.row = row
+        self.col = col
+
+    def __str__(self):
+        return f"[{self.row}, {self.col}]"
+
 
 class Board:
     def __init__(self):
@@ -54,7 +89,9 @@ class Board:
                     continue
                 row = []
                 for letter in line.strip().upper().split(' '):
-                    if letter == '-' or letter.isupper():
+                    if letter == '-':
+                        row.append('')
+                    elif letter.isupper():
                         row.append(letter)
                     else:
                         raise ValueError(f"Invalid input letter: {letter}")
@@ -63,6 +100,47 @@ class Board:
                 self.state.append(row)
             if len(self.state) != self.num_rows:
                 raise ValueError(f"Expected {self.num_rows} rows, got {len(self.state)}")
+
+    def are_adjacent_tiles_empty(self, position: Position) -> bool:
+        # Top
+        if position.row != 0 and self.state[position.row - 1][position.col] != '':
+            return False
+        # Bottom
+        if position.row != (self.num_rows - 1) and self.state[position.row + 1][position.col] != '':
+            return False
+        # Left
+        if position.col != 0 and self.state[position.row][position.col - 1] != '':
+            return False
+        # Right
+        if position.col != (self.num_cols - 1) and self.state[position.row][position.col + 1] != '':
+            return False
+        return True
+
+    def is_state_valid(self, dictionary: Dictionary) -> bool:
+        # Check that all tiles are next to at least one other tile.
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                if self.state[row][col] != '' and self.are_adjacent_tiles_empty(Position(row, col)):
+                    return False
+        # Check rows.
+        for row in self.state:
+            chunks = ''.join(row).split('-')
+            chunks = filter(lambda x: len(x) > 1, chunks)
+            if not all(map(lambda x: dictionary.is_word(x), chunks)):
+                return False
+        # Check columns.
+        for col_num in range(self.num_cols):
+            chunks = ''.join(self.get_column(col_num)).split('-')
+            chunks = filter(lambda x: len(x) > 1, chunks)
+            if not all(map(lambda x: dictionary.is_word(x), chunks)):
+                return False
+        return True
+
+    def get_column(self, index) -> List[str]:
+        col = []
+        for row in self.state:
+            col.append(row[index])
+        return col
 
     def save(self, filepath: str):
         pass
@@ -74,7 +152,6 @@ class Board:
             output += '\n'
         return output
 
-
 def get_all_substrings(word: str) -> List[str]:
     substrings = []
     for substring_len in range(2, len(word)):
@@ -82,47 +159,29 @@ def get_all_substrings(word: str) -> List[str]:
             substrings.append(word[start_index:(start_index + substring_len)])
     return substrings
 
-class Dictionary:
-    def __init__(self):
-        self.words = set()
-        self.substrings = set()
-
-    def load(self, filepath: str):
-        with open(filepath, 'r') as file:
-            for line in file.readlines():
-                if line == '':
-                    continue
-                self.words.add(line.strip().upper())
-        self.preprocess()
-        print(f"Loaded {len(self.words)} words")
-        print(f"Preprocessed {len(self.substrings)} substrings")
-
-    def preprocess(self):
-        for word in self.words:
-            for substring in get_all_substrings(word):
-                self.substrings.add(substring)
-
 def read_directory_files(path):
     files = set()
     for filename in os.listdir(path):
         files.add(filename)
     return files
 
-def read_state(path):
-    state = []
-    return state
-
-PATH = '/Users/pvishayanuroj/projects/scrabble/dictionaries'
-
-#DICTIONARY_PATH = '/Users/pvishayanuroj/projects/scrabble/dictionaries/10k-dictionary.txt'
-DICTIONARY_PATH = '/Users/pvishayanuroj/projects/scrabble/dictionaries/178k-dictionary.txt'
+#DICTIONARIES_PATH = '/Users/pvishayanuroj/projects/scrabble/dictionaries'
+DICTIONARY_PATH = '/Users/pvishayanuroj/projects/scrabble/dictionaries/10k-dictionary.txt'
+#DICTIONARY_PATH = '/Users/pvishayanuroj/projects/scrabble/dictionaries/178k-dictionary.txt'
 BOARD_PATH = '/Users/pvishayanuroj/projects/scrabble/boards/official.txt'
 STATE_PATH = '/Users/pvishayanuroj/projects/scrabble/states/test.txt'
 
 board = Board()
 board.init_board(BOARD_PATH)
 board.load_state(STATE_PATH)
-print(board)
+
 #files = read_directory_files(PATH)
-#dictionary = Dictionary()
-#dictionary.load(DICTIONARY_PATH)
+dictionary = Dictionary()
+dictionary.load(DICTIONARY_PATH)
+
+is_valid = board.is_state_valid(dictionary)
+print(f"is board valid: {is_valid}")
+
+
+def solver(dictionary: Dictionary, board: Board, tiles: List[str]):
+    pass
