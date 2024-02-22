23329296f4c451cc21ba56145fc13446d0f754b1
Paul Vishayanuroj <pvishayanuroj@gmail.com>
2024-02-23 01:17:54 +0900

    Word scorer.

---
+++ python/word_position.py
@@ -0,0 +1,24 @@
+from position import Position
+from enums import Shape
+
+
+class WordPosition:
+    def __init__(self, position: Position, word: str, shape: Shape):
+        self._position = position
+        self._word = word
+        self._shape = shape
+
+    @property
+    def position(self) -> Position:
+        return self._position
+
+    @property
+    def word(self) -> str:
+        return self._word
+
+    @property
+    def shape(self) -> str:
+        return self._shape
+
+    def __str__(self):
+        return f"{self._word} at {self._position}: {self._shape}"
