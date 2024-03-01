4c82a0f43dbabc3912c2c5d1da4d83c099021235
Paul Vishayanuroj <pvishayanuroj@gmail.com>
2024-03-02 00:03:13 +0900

    Fixed a bug with how the score was calculated when all tiles were used up.

--- python/scoreboard.py
+++ python/scoreboard.py
@@ -70,7 +70,7 @@ class Scoreboard:
             total += (letter_value * letter_multiplier)
             curr_position = curr_position.move(direction)
         total *= word_multipler
-        if len(word_position.word) == MAX_LENGTH_WORD:
+        if len(active_tiles) == MAX_LENGTH_WORD:
             total += MAX_LENGTH_WORD_SCORE
         return total
 
