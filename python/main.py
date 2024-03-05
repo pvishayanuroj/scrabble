d3f07c90853f2299a57ea9d57c3a24fd790a4348
Paul Vishayanuroj <pvishayanuroj@gmail.com>
2024-03-06 08:55:16 +0900

    Updated omit dictionary.

--- python/main.py
+++ python/main.py
@@ -77,15 +77,15 @@ def main():
         # selected_solution.save(generate_file_name(args.games, game_name))
     elif selection == MenuSelection.LOAD_GAME:
         game_names = get_games(args.games)
-        #game_name = select_game_name(game_names)
-        #game_file = get_latest_game_file(args.games, game_name)
-        #player_tiles = get_player_tiles()
-        # if player_tiles is None:
-        #     return
+        game_name = select_game_name(game_names)
+        game_file = get_latest_game_file(args.games, game_name)
+        player_tiles = get_player_tiles()
+        if player_tiles is None:
+            return
         # game_file = '/Users/pvishayanuroj/projects/scrabble/games/game2_20240301_000000.txt'
         # player_tiles = PlayerTiles('GETHUTO')
-        game_file = '/Users/pvishayanuroj/projects/scrabble/games/game3_20240301_000000.txt'
-        player_tiles = PlayerTiles('NRALEFI')
+        #game_file = '/Users/pvishayanuroj/projects/scrabble/games/game3_20240301_000000.txt'
+        #player_tiles = PlayerTiles('NRALEFI')
         scoreboard = Scoreboard(args.board, args.points)
         dictionary = Dictionary(args.dictionary, args.omit)
         board = Board(scoreboard.size, dictionary)
